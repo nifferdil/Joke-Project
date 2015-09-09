837095f67d5b35794f5fd9c37a82cac39d92a1d1
Nataliya Miller <nataliyamiller82@gmail.com>
2015-09-09 14:02:03 -0700

    Category class and tests

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -21,4 +21,52 @@ public class CategoryTest {
     assertTrue(firstCategory.equals(secondCategory));
   }
 
+  @Test
+  public void save_savesIntoDatabase_true() {
+    Category myCategory = new Category("Pun", "Spain");
+    myCategory.save();
+    assertTrue(Category.all().get(0).equals(myCategory));
+  }
+
+  @Test
+  public void find_findAuthorInDatabase_true() {
+    Category myCategory = new Category("Pun", "Spain");
+    myCategory.save();
+    Category savedCategory = Category.find(myCategory.getId());
+    assertTrue(myCategory.equals(savedCategory));
+  }
+
+  @Test
+  public void addJoke_addsJokeToCategory() {
+    // Category myCategory = new Category("Riddle", "Holiday");
+    // myCategory.save();
+    // Joke myJoke = new Joke("Knock Knock", "Orange you glad", 1);
+    // myJoke.save();
+    // myCategory.addJoke(myJoke);
+    // Joke savedJoke = myCategory.getJokes().get(0);
+    // assertTrue(myJoke.equals(savedJoke));
+  }
+
+  @Test
+  public void getJokes_returnsAllJokes_ArrayList() {
+    // Category myCategory = new Category("Riddle", "Holiday");
+    // myCategory.save();
+    // Joke myJoke = new Joke("Knock Knock", "Orange you glad", 1);
+    // myJoke.save();
+    // myCategory.addJoke(myJoke);
+    // List savedJokes = myCategory.getJokes();
+    // assertEquals(savedJokes.size(), 1);
+  }
+
+  @Test
+  public void delete_deletesAllJokesAndListAssoications() {
+    // Category myCategory = new Category("Riddle", "Holiday");
+    // myCategory.save();
+    // Joke myJoke = new Joke("Knock Knock", "Orange you glad", 1);
+    // myJoke.save();
+    // myCategory.addJoke(myJoke);
+    // myCategory.delete();
+    // assertEquals(myJoke.getCategories().size(), 0);
+  }
+
 }
