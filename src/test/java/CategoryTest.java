a35e2fcd79e08aca724f5e669f8144d29172ad28
Diana Holland <holla@bikefarm.org>
2015-09-09 11:30:21 -0700

    Initial tests are passing

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -9,5 +9,16 @@ public class CategoryTest {
   public DatabaseRule database = new DatabaseRule();
 
   @Test
-  
+  public void category_instantiates_True() {
+    Category testCategory = new Category("Pun", "Spain");
+    assertTrue(testCategory instanceof Category);
+  }
+
+  @Test
+  public void equals_returnsTrueIfCategoriesAretheSame_true() {
+    Category firstCategory = new Category("Pun", "Spain");
+    Category secondCategory = new Category("Pun", "Spain");
+    assertTrue(firstCategory.equals(secondCategory));
+  }
+
 }
